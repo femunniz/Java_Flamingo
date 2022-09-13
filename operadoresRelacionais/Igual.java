d35a8ebb47f68e1b200ab8622397912ca5791704
Felipe Muniz <101819538+femunniz@users.noreply.github.com>
2022-09-13 20:38:24 -0300

    Add files via upload

---
+++ operadoresRelacionais/Igual.java
@@ -0,0 +1,15 @@
+package operadoresRelacionais;
+
+public class Igual {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 20;
+
+        if (x == y) {
+            System.out.println("São iguais !");
+        } else {
+            System.out.println("São diferentes !");
+        }
+    }
+}
