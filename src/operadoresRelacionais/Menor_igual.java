5b70e0c1a81ce16b6a4cce0e47744f5a98dcd706
Felipe Muniz <101819538+femunniz@users.noreply.github.com>
2022-09-13 20:39:06 -0300

    Add files via upload

---
+++ src/operadoresRelacionais/Menor_igual.java
@@ -0,0 +1,10 @@
+package operadoresRelacionais;
+
+public class Menor_igual {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        System.out.println(x <= y);
+    }
+}
